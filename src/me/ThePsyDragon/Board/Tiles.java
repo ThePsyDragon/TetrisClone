e71f9411e833c9eda452d416580cabd7fd083cdc
ThePsyDragon <thepsydragon@gmail.com>
2013-12-13 11:19:35 -0600

    12-13-13 First
    Updated Structure.

---
+++ src/me/ThePsyDragon/Board/Tiles.java
@@ -0,0 +1,20 @@
+package me.ThePsyDragon.Board;
+
+/*
+Description: 
+  Static
+    - Contains an ArrayList representing each game board tile.
+    - Enumeration of Tile Types
+    - All Available tile Textures
+  Object
+    - Object Represents status of a single game board tile
+    Attributes
+      - Tile Type
+    Functions
+      - Draw
+ */
+public class Tiles {
+	//Global Variables
+	  //Array of all Tiles; X,Y
+	  Tiles[][] Tiles = new Tiles[10][20];
+}
