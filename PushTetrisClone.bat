70ac254dcf6cd40b5092410942d7d874f235e436
ThePsyDragon <thepsydragon@gmail.com>
2013-12-13 13:00:47 -0600

    12-13-13 Third. Began Work on framework for Rendered Objects.

--- PushTetrisClone.bat
+++
@@ -1,4 +0,0 @@
-cd I:\"Personal Programming"\workspace\TetrisClone
-git remote add TetrisClone https://github.com/ThePsyDragon/TetrisClone.git
-git push TetrisClone master "Test Commit"
-PAUSE
--- src/me/ThePsyDragon/Board/Tiles.java
+++ src/me/ThePsyDragon/Board/Tiles.java
@@ -1,5 +1,7 @@
 package me.ThePsyDragon.Board;
 
+import me.ThePsyDragon.TetrisClone.GameObject;
+
 /*
 Description: 
   Static
@@ -10,13 +12,34 @@ Description:
     - Object Represents status of a single game board tile
     Attributes
       - Tile Type
+      - Tile Texture
     Functions
-      - Draw
+      - getType()
  */
-public class Tiles {
+public class Tiles extends GameObject{
 	//Global Variables
-	  //Array of all Tiles; X,Y
-	  Tiles[][] Tiles = new Tiles[10][20];
-	  //Enumeration of Tile Types
-	  public static int empty = 1;
+	  //Static
+	    //Public
+	      //Array of all Tiles; X,Y
+	      public static Tiles[][] Tiles = new Tiles[10][20];
+	      //Enumeration of Tile Types
+	      public static int Empty = 1;
+	      public static int OBlock = 2;
+	      public static int TBlock = 3;
+	      public static int SBlock = 4;
+	      public static int ZBlock = 5;
+	      public static int IBlock = 6;
+	      public static int JBlock = 7;
+	      public static int LBlock = 8;
+	      //Block Textures
+	      private static String OBlockTex = "";
+	      private static String TBlockTex = "";
+	      private static String SBlockTex = "";
+	      private static String ZBlockTex = "";
+	      private static String IBlockTex = "";
+	      private static String JBlockTex = "";
+	      private static String LBlockTex = "";
+	      private static String EmptyTex = "";
+	  //Non-Static
+	    //Private
 }
