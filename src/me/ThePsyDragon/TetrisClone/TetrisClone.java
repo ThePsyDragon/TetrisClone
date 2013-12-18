618c209402d1e883421577637ccf2d25b3713af0
ThePsyDragon <thepsydragon@gmail.com>
2013-12-17 22:12:52 -0600

    12-17-13 Final. Updated gitignore and .classpath to allow for seamless transfer of dependent Libraries. Happy B-day Mom :)

--- src/me/ThePsyDragon/TetrisClone/TetrisClone.java
+++ src/me/ThePsyDragon/TetrisClone/TetrisClone.java
@@ -22,6 +22,9 @@ public class TetrisClone {
 	int tickLength = 50;
 	int tpsCounter = 0;
 	long totalTPSCounter = 0;
+	long tpsTimer = 0;
+	long tpsTimer2 = 0;
+	long tickTimer = 0;
 	double currentTPS = 0;
 	double averageTPS = 0;
 	boolean debug = false;
@@ -31,8 +34,13 @@ public class TetrisClone {
 
 	public static void main(String[] args) {
 		// Variables
-		// Start Program
 		TetrisClone tc = new TetrisClone();
+		// Start Program
+		if(args.length >0){
+			if(args[0].equalsIgnoreCase("debug")){
+				tc.debug = true;
+			}
+		}
 		tc.Start();
 	}
 
@@ -53,32 +61,10 @@ public class TetrisClone {
 		glOrtho(0, 720, 0, 720, 1, -1);
 		glMatrixMode(GL_MODELVIEW);
 		// Begin Loop
-		long tpsTimer2 = 0;
-		long tpsTimer = System.currentTimeMillis();
-		long tickTimer = System.currentTimeMillis();
+		
 		while (!Display.isCloseRequested()) {
-			// Check TPS
-			if (TPSRestrict) {
-				while (System.currentTimeMillis() - tickTimer < tickLength) {
-					// Pause till tick is completed
-				}
-			}
-			tickTimer = System.currentTimeMillis();
-			// Calculate tps
-			tpsTimer2 = System.currentTimeMillis();
-			tpsCounter += 1;
-			this.print("TPS Counter: " + tpsCounter);
-			if (tpsTimer2 - tpsTimer > 1000) {
-				double difference = (double) (tpsTimer2 - tpsTimer);
-				this.print("" + difference);
-				currentTPS = (tpsCounter / difference) * 1000;
-				this.print("Current TPS: " + currentTPS);
-				tpsCounter = 0;
-				totalTPSCounter += 1;
-				averageTPS = ((averageTPS * (totalTPSCounter - 1)) + currentTPS)
-						/ totalTPSCounter;
-				tpsTimer = System.currentTimeMillis();
-			}
+			//TPS Calculations
+			TPSCalc();
 			// Update Positions
 			// Render
 			for (int i = 0; i < RendObjList.size(); i++) {
@@ -101,5 +87,33 @@ public class TetrisClone {
 		if (debug) {
 			System.out.println(message);
 		}
+	}
+	
+	public void TPSCalc(){
+		tpsTimer2 = 0;
+		tpsTimer = System.currentTimeMillis();
+		tickTimer = System.currentTimeMillis();
+		// Check TPS
+					if (TPSRestrict) {
+						while (System.currentTimeMillis() - tickTimer < tickLength) {
+							// Pause till tick is completed
+						}
+					}
+					tickTimer = System.currentTimeMillis();
+					// Calculate tps
+					tpsTimer2 = System.currentTimeMillis();
+					tpsCounter += 1;
+					this.print("TPS Counter: " + tpsCounter);
+					if (tpsTimer2 - tpsTimer > 1000) {
+						double difference = (double) (tpsTimer2 - tpsTimer);
+						this.print("" + difference);
+						currentTPS = (tpsCounter / difference) * 1000;
+						this.print("Current TPS: " + currentTPS);
+						tpsCounter = 0;
+						totalTPSCounter += 1;
+						averageTPS = ((averageTPS * (totalTPSCounter - 1)) + currentTPS)
+								/ totalTPSCounter;
+						tpsTimer = System.currentTimeMillis();
+					}
 	}
 }
